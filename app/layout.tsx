66c8448c19535f031222af493c800894583aa4de
GiFTED <gkenny896@gmail.com>
2025-10-28 19:55:23 +0100

    fix: theme color update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,32 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  themeColor: "#000000",
   title: "Xend Global",
   description: "Stablecoin powered internet",
+  twitter: {
+    card: "summary_large_image",
+    title: "Xend Global",
+    description: "Stablecoin powered internet",
+    site: "https://xend.global",
+    creator: "@xend_global",
+    images: ["/images/og-image.png"],
+  },
+  openGraph: {
+    title: "Xend Global",
+    description: "Stablecoin powered internet",
+    siteName: "Xend Global",
+    images: ["/images/og-image.png"],
+  },
+  keywords: [
+    "Xend",
+    "Xend Global",
+    "Stablecoin",
+    "Internet",
+    "Payments",
+    "Socials",
+  ],
+  appLinks: {},
 };
 
 export default function RootLayout({
